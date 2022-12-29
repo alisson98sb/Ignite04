e0ed57403abf32919f73954737db59b4d3a9060b
alisson98sb <alisson98sb@gmail.com>
2022-12-29 12:59:45 -0300

    Applying typescript

--- src/components/ModalEditFood/index.tsx
+++ src/components/ModalEditFood/index.tsx
@@ -1,44 +1,57 @@
-import { useRef } from 'react';
-import { FiCheckSquare } from 'react-icons/fi';
+import { useRef } from "react";
+import { FiCheckSquare } from "react-icons/fi";
 
-import { Form } from './styles';
-import {Modal} from '../Modal';
-import Input from '../Input';
+import { Form } from "./styles";
+import { Modal } from "../Modal";
+import Input from "../Input";
 
 interface ModalEditProps {
   isOpen: boolean;
-  setIsOpen: ()=> void;
-  editingFood: object;
-  handleUpdateFood: (food: object) => Promise<void>;
+  setIsOpen: () => void;
+  editingFood: FoodProps;
+  handleUpdateFood: (food: FoodProps) => Promise<void>;
 }
 
-export function ModalEditFood({isOpen,setIsOpen,editingFood,handleUpdateFood}: ModalEditProps) {
+interface FoodProps {
+  available: boolean;
+  id: number;
+  image: string;
+  name: string;
+  description: string;
+  price: string;
+}
+
+export function ModalEditFood({
+  isOpen,
+  setIsOpen,
+  editingFood,
+  handleUpdateFood,
+}: ModalEditProps) {
   const formRef = useRef(null);
 
-  function handleSubmit(data: object) {
+  function handleSubmit(data: FoodProps) {
     handleUpdateFood(data);
     setIsOpen();
-  };
+  }
 
-    return (
-      <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-        <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
-          <h1>Editar Prato</h1>
-          <Input name="image" placeholder="Cole o link aqui" />
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
 
-          <Input name="name" placeholder="Ex: Moda Italiana" />
-          <Input name="price" placeholder="Ex: 19.90" />
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
 
-          <Input name="description" placeholder="Descrição" />
+        <Input name="description" placeholder="Descrição" />
 
-          <button type="submit" data-testid="edit-food-button">
-            <div className="text">Editar Prato</div>
-            <div className="icon">
-              <FiCheckSquare size={24} />
-            </div>
-          </button>
-        </Form>
-      </Modal>
-    );
-};
-
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+}
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import {Header} from '../../components/Header';
-import api from '../../services/api';
-import {Food} from '../../components/Food';
-import {ModalAddFood} from '../../components/ModalAddFood';
-import {ModalEditFood} from '../../components/ModalEditFood';
-import { FoodsContainer } from './styles';
+import { Header } from "../../components/Header";
+import api from "../../services/api";
+import { Food } from "../../components/Food";
+import { ModalAddFood } from "../../components/ModalAddFood";
+import { ModalEditFood } from "../../components/ModalEditFood";
+import { FoodsContainer } from "./styles";
 
 interface FoodProps {
   available: boolean;
@@ -14,24 +14,21 @@ interface FoodProps {
   name: string;
   description: string;
   price: string;
-};
+}
 
 export function Dashboard() {
-
   const [foods, setFoods] = useState<FoodProps[]>([]);
-  const [editingFood, setEditingFood] = useState<FoodProps>();
+  const [editingFood, setEditingFood] = useState<any>({});
   const [modalOpen, setModalOpen] = useState(false);
   const [editModalOpen, setEditModalOpen] = useState(false);
 
   useEffect(() => {
-    api
-      .get('/foods')
-      .then((response) => setFoods(response.data));
+    api.get("/foods").then((response) => setFoods(response.data));
   }, []);
 
   async function handleAddFood(food: FoodProps) {
     try {
-      const response = await api.post('/foods', {
+      const response = await api.post("/foods", {
         ...food,
         available: true,
       });
@@ -42,18 +39,23 @@ export function Dashboard() {
     }
   }
 
+  async function handleEditFood(food: FoodProps) {
+    setEditingFood(food);
+    setEditModalOpen(true);
+  }
+
   async function handleUpdateFood(food: FoodProps) {
     try {
-      const foodUpdated = await api.put(
-        `/foods/${editingFood.id}`,
-        { ...editingFood, ...food },
+      const foodUpdated = await api.put(`/foods/${editingFood.id}`, {
+        ...editingFood,
+        ...food,
+      });
+
+      const foodsUpdated = foods.map((f) =>
+        f.id !== foodUpdated.data.id ? f : foodUpdated.data
       );
 
-      const foodsUpdated = foods.map(f =>
-        f.id !== foodUpdated.data.id ? f : foodUpdated.data,
-      );
-
-      setFoods(foodsUpdated)
+      setFoods(foodsUpdated);
     } catch (err) {
       console.log(err);
     }
@@ -62,9 +64,9 @@ export function Dashboard() {
   async function handleDeleteFood(id: number) {
     await api.delete(`/foods/${id}`);
 
-    const foodsFiltered = foods.filter(food => food.id !== id);
+    const foodsFiltered = foods.filter((food) => food.id !== id);
 
-    setFoods(foodsFiltered)
+    setFoods(foodsFiltered);
   }
 
   function toggleModal() {
@@ -72,43 +74,35 @@ export function Dashboard() {
   }
 
   function toggleEditModal() {
-    setEditModalOpen(!editModalOpen)
+    setEditModalOpen(!editModalOpen);
   }
 
-  function handleEditFood(food: FoodProps) {
-    setEditingFood(food);
-    setEditModalOpen(true);
-  }
+  return (
+    <>
+      <Header openModal={toggleModal} />
+      <ModalAddFood
+        isOpen={modalOpen}
+        setIsOpen={toggleModal}
+        handleAddFood={handleAddFood}
+      />
+      <ModalEditFood
+        isOpen={editModalOpen}
+        setIsOpen={toggleEditModal}
+        editingFood={editingFood}
+        handleUpdateFood={handleUpdateFood}
+      />
 
-    // const { modalOpen, editModalOpen, editingFood, foods } = this.state;
-
-    return (
-      <>
-        <Header openModal={toggleModal} />
-        <ModalAddFood
-          isOpen={modalOpen}
-          setIsOpen={toggleModal}
-          handleAddFood={handleAddFood}
-        />
-        <ModalEditFood
-          isOpen={editModalOpen}
-          setIsOpen={toggleEditModal}
-          editingFood={editingFood}
-          handleUpdateFood={handleUpdateFood}
-        />
-
-        <FoodsContainer data-testid="foods-list">
-          {foods &&
-            foods.map(food => (
-              <Food
-                key={food.id}
-                food={food}
-                handleDelete={handleDeleteFood}
-                handleEditFood={handleEditFood}
-              />
-            ))}
-        </FoodsContainer>
-      </>
-    );
-};
-
+      <FoodsContainer data-testid="foods-list">
+        {foods &&
+          foods.map((food) => (
+            <Food
+              key={food.id}
+              food={food}
+              handleDelete={handleDeleteFood}
+              handleEditFood={handleEditFood}
+            />
+          ))}
+      </FoodsContainer>
+    </>
+  );
+}
